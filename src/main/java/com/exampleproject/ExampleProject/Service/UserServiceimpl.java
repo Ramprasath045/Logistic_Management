d0d3d95ba55cb9333d63bc33d06d6af0ede583bb
unknown <ramprasath045@gmail.com>
2024-02-26 18:31:15 +0530

    Git push from Backend Team

---
+++ src/main/java/com/exampleproject/ExampleProject/Service/UserServiceimpl.java
@@ -0,0 +1,28 @@
+package com.exampleproject.ExampleProject.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.exampleproject.ExampleProject.Dto.UserDto;
+import com.exampleproject.ExampleProject.Model.User;
+import com.exampleproject.ExampleProject.Repository.UserRepository;
+
+@Service
+public class UserServiceimpl implements UserService{
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public User save(UserDto userDto) {
+		User user = new User(userDto.getEmail(), passwordEncoder.encode(userDto.getPassword()), userDto.getRole(), userDto.getFullname());
+		return userRepository.save(user);
+	}
+	
+	 
+}
